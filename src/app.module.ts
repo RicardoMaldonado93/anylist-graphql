f98254643e1ae0637d782ffcacfce93840dbd2ac
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2022-11-02 09:31:20 -0300

    feat: added user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
 import { ItemsModule } from './items/items.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { ItemsModule } from './items/items.module';
       synchronize: process.env.STATE === 'dev',
     }),
     ItemsModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
